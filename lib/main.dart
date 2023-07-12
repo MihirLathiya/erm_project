98b0c347bdb090e4d975f28b09b5edf03c5b0307
lathi <lathiyamihir72@gmail.com>
2023-07-12 22:33:13 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           getPages: getPages,
           unknownRoute: GetPage(name: "/not-found", page: () => SplashScreen()),
-          initialRoute: AppRoutes.splashScreen,
+          initialRoute: AppRoutes.homeScreen,
         );
       },
     );
